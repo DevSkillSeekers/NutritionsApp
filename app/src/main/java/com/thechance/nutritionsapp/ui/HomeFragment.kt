d97d901310dbc4c1d1844b0190091005d59d72d1
Nada Feteiha <nada.feteiha@gmail.com>
2022-07-13 03:26:19 -0400

    edit in homeScreen

--- app/src/main/java/com/thechance/nutritionsapp/ui/HomeFragment.kt
+++ app/src/main/java/com/thechance/nutritionsapp/ui/HomeFragment.kt
@@ -3,12 +3,8 @@ package com.thechance.nutritionsapp.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.thechance.nutritionsapp.BaseFragment
-import com.thechance.nutritionsapp.R
-import com.thechance.nutritionsapp.data.DataManger
 import com.thechance.nutritionsapp.databinding.FragmentHomeBinding
-import com.thechance.nutritionsapp.util.Constants
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
@@ -16,13 +12,14 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         FragmentHomeBinding::inflate
 
     override fun setup() {
+
 //        binding.ivText.text = DataManger.getBreakfast().toString()
-        binding.button.setOnClickListener {
-            changeFragment(DetailMealFragment())
-        }
+//        binding.button.setOnClickListener {
+//            changeFragment(DetailMealFragment())
+//        }
     }
 
-    private fun changeFragment(fragment: Fragment) {
+    /*private fun changeFragment(fragment: Fragment) {
         val healthMeal = DataManger.getBreakfast().first()
         val data = Bundle()
         data.putParcelable(Constants.EXTRA_MEAL_DETAILS, healthMeal)
@@ -30,5 +27,5 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
         val transaction = requireActivity().supportFragmentManager.beginTransaction()
         transaction.add(R.id.nav_host_fragment, fragment).addToBackStack(null)
         transaction.commit()
-    }
+    }*/
 }
