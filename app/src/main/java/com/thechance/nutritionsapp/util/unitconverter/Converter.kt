369bb0b1886207cd2e77b08b20bc206321c7ad8d
alkomity <197023.it.eng@student.aden-univ.net>
2022-07-19 10:45:42 +0300

    update whole converter class by make each function has one function and write test cases for its

--- app/src/main/java/com/thechance/nutritionsapp/util/unitconverter/Converter.kt
+++ app/src/main/java/com/thechance/nutritionsapp/util/unitconverter/Converter.kt
@@ -1,30 +1,26 @@
 package com.thechance.nutritionsapp.util.unitconverter
 
 class Converter {
-    private val weight = mapOf(
-        "Kilogram" to 0.001,
-        "Gram" to 1.0,
-        "Milligram" to 1000.0,
-        "Pound" to 0.00220462,
-        "cm" to 1.div(30.48),
-        "feet" to 30.48
-    )
+    fun kiloToGram(kilo : Double) : Double {
+        return kilo * 1000
+    }
 
-    fun convertFromTypeToOther(
-        converterType: String,
-        convertFrom: String,
-        convertTo: String,
-        amount: Double
-    ): Double? {
-        return when {
-            amount < 0 -> {
-                null
-            }
-            else -> when (converterType) {
-                //Because maybe we need add other converters
-                ConverterUtil.MainConverter.weight -> amount * weight[convertTo]!! / weight[convertFrom]!!
-                else -> 0.0
-            }
-        }
+    fun kiloToPound(kilo: Double) : Double {
+        return kilo * 2.20462
+    }
+
+    fun gramToKilo(gram : Double) : Double {
+        return gram / 1000
+    }
+
+    fun gramToPound(gram : Double) : Double {
+        return gramToKilo(gram) * 2.20462
+    }
+
+    fun poundToKilo(pound : Double) : Double {
+        return pound / 2.20462
+    }
+    fun poundToGram(pound: Double) : Double {
+        return pound * 453.592
     }
 }
