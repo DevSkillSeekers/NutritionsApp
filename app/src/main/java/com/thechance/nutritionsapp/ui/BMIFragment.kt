220a58fee5d7d70935bd119c55948f429f88bb5c
Dunya_4Mar <88485820+Dunya4mars@users.noreply.github.com>
2022-07-22 00:06:26 +0300

    change_use_fun_convertutil_to_convert

--- app/src/main/java/com/thechance/nutritionsapp/ui/BMIFragment.kt
+++ app/src/main/java/com/thechance/nutritionsapp/ui/BMIFragment.kt
@@ -53,7 +53,7 @@ class BMIFragment : BaseFragment<FragmentBMIBinding>() {
                 val height = binding.heightTIET.text.toString().toDoubleOrNull() ?: 0.0
                 val weighUnit = binding.weightInputView.text.toString()
                 val heightUnit = binding.heightInputView.text.toString()
-           //    calculateBMI(weight, height, weighUnit, heightUnit)
+             calculateBMI(weight, height, weighUnit, heightUnit)
             } else {
                 Toast.makeText(context, "Please enter your  weight:between(40-132)  and  height:between(140-210)", Toast.LENGTH_LONG)
                     .show()
@@ -68,59 +68,50 @@ class BMIFragment : BaseFragment<FragmentBMIBinding>() {
         }
     }
 
-//    private fun calculateBMI(
-//        weight: Double,
-//        height: Double,
-//        weighUnit: String,
-//        heightUnit: String
-//    ) {
-//        var heightCM = height
-//        if (heightUnit != resources.getString(R.string.cm_text)) {
-//            heightCM = Converter().convertFromTypeToOther(
-//                Converter.MainConverter.weight,
-//                "feet",
-//                "cm",
-//                height
-//            ) ?: height
-//        }
-//        var weightKG = weight
-//        if (weighUnit != resources.getString(R.string.kg_text)) {
-//            weightKG = Converter().convertFromTypeToOther(
-//                ConverterUtil.MainConverter.weight,
-//                "Pound",
-//                "Kilogram",
-//                height
-//            ) ?: weight
-//        }
-//
-//        // display the result
-//        binding.displayTipsTV.movementMethod = ScrollingMovementMethod()
-//        val bmiCalculator = BMI(weightKG, heightCM)
-//        val result = bmiCalculator.calculation0fBMI()
-//
-//        if (result != -1.0) {
-//            binding.ArcProgress.progress = result.toInt()
-//            val type = bmiCalculator.diagnostic(result)
-//            binding.displayResultTv.text = type
-//            binding.displayNumResultTv.text=result.toString()
-//            when (result) {
-//                in 0.0..18.49 -> {
-//                    binding.ArcProgress.finishedStrokeColor=(resources.getColor(R.color.color_yellow))
-//                    binding.displayTipsTV.setText(R.string.tips_low_weight)
-//                    binding.displayResultTv.setTextColor(resources.getColor(R.color.color_yellow))}
-//                in 18.50..24.99 -> {
-//                        binding.ArcProgress.finishedStrokeColor = (resources.getColor(R.color.blue))
-//                        binding.displayTipsTV.setText(R.string.tips_normal_weight)
-//                        binding.displayResultTv.setTextColor(resources.getColor(R.color.blue))
-//                }
-//
-//                in 25.0..40.0 -> {
-//                    binding.ArcProgress.finishedStrokeColor = (resources.getColor(R.color.color_red))
-//                    binding.displayTipsTV.setText(R.string.tips_over_weight)
-//                    binding.displayResultTv.setTextColor(resources.getColor(R.color.color_red))}
-//                }
-//            }else{
-//            Toast.makeText(context, "Please make sure that you enter the correct values ", Toast.LENGTH_LONG).show()
-//            }
-//    }
+    private fun calculateBMI(
+        weight: Double,
+        height: Double,
+        weighUnit: String,
+        heightUnit: String
+    ) {
+        var heightCM = height
+        if (heightUnit != resources.getString(R.string.cm_text)) {
+            heightCM = Converter().feetToCm(height)
+        }
+
+        var weightKG = weight
+        if (weighUnit != resources.getString(R.string.kg_text)) {
+            weightKG = Converter().poundToKilo(weight)
+        }
+
+        // display the result
+        binding.displayTipsTV.movementMethod = ScrollingMovementMethod()
+        val bmiCalculator = BMI(weightKG, heightCM)
+        val result = bmiCalculator.calculation0fBMI()
+
+        if (result != -1.0) {
+            binding.ArcProgress.progress = result.toInt()
+            val type = bmiCalculator.diagnostic(result)
+            binding.displayResultTv.text = type
+            binding.displayNumResultTv.text=result.toString()
+            when (result) {
+                in 0.0..18.49 -> {
+                    binding.ArcProgress.finishedStrokeColor=(resources.getColor(R.color.color_yellow))
+                    binding.displayTipsTV.setText(R.string.tips_low_weight)
+                    binding.displayResultTv.setTextColor(resources.getColor(R.color.color_yellow))}
+                in 18.50..24.99 -> {
+                        binding.ArcProgress.finishedStrokeColor = (resources.getColor(R.color.blue))
+                        binding.displayTipsTV.setText(R.string.tips_normal_weight)
+                        binding.displayResultTv.setTextColor(resources.getColor(R.color.blue))
+                }
+
+                in 25.0..40.0 -> {
+                    binding.ArcProgress.finishedStrokeColor = (resources.getColor(R.color.color_red))
+                    binding.displayTipsTV.setText(R.string.tips_over_weight)
+                    binding.displayResultTv.setTextColor(resources.getColor(R.color.color_red))}
+                }
+            }else{
+            Toast.makeText(context, "Please make sure that you enter the correct values ", Toast.LENGTH_LONG).show()
+            }
+    }
 }
